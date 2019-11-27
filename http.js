ec627c00159c1b5de91af2c234b6daaf3a55cdf8
lleal24 <lorenaleal24@gmail.com>
2019-11-27 11:35:59 -0800

    Modulo para implementar la creacion de un servidor en node js

--- http.js
+++ http.js
@@ -1,17 +1,20 @@
 /* creacion de servidor */
 
 const http = require('http');
+const port = 3030;
 
-/* const server = http.createServer();
+/* FIRTS SESSION
+const server = http.createServer();
 
 server.on('connection', (socket)=>{ uso de sockets
     console.log('nueva conexion detectada');
 })
 
-server.listen(2012) servidor en modo escucha
-console.log('Escuchando en el puerto 2012...'); */
+server.listen(port) servidor en modo escucha
+console.log('Escuchando en el puerto' +port+'...'); */
+/* --------------------------------------------------------------------------------- */
 
-const port = 3030;
+/* SECOND SESSION
 const server = http.createServer((req, res)=>{
     if(req.url === "/"){
         res.write('Hola Mundo desde node js');
@@ -19,7 +22,7 @@ const server = http.createServer((req, res)=>{
         //indicar el final de la respuesta
         res.end();
     }
-    /* req.url = se obtiene la url de la peticion */
+     req.url = se obtiene la url de la peticion 
     if(req.url == '/coches'){
         res.write('Pagina de coches');
         res.end();
@@ -27,4 +30,17 @@ const server = http.createServer((req, res)=>{
     }
 });
 server.listen(port);
-console.log('Escuchando en puerto '+port);
+console.log('Escuchando en puerto '+port); */
+
+/* --------------------------------------------------------------------------------- */
+const server = http.createServer((req, res)=>{
+    res.writeHead(
+        200, //status code
+        {'Content-Type': 'text/html'} //tipo de contenido
+    );
+    res.write('<h1>Esta es mi pagina</h1>');
+    res.write('<p>Este es mi parrado</p>');
+    res.end();
+}).listen(port);
+console.log('Server listen on port: '+port+'...')
+
